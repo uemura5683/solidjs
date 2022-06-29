d1a8256d4473bcd5d97d720b77a8b2cbfa946b2f
uemu5683 <nobuyoshi5683@gmail.com>
2022-06-30 00:12:32 +0900

    solidjsを学習

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 import Nested from './nested';
-import { createSignal } from "solid-js";
+import { createSignal, createEffect } from "solid-js";
 
 function HelloWorld() {
   const name = "Solid";
@@ -19,7 +19,9 @@ function HelloWorld() {
   )
 
   /**
-   * 時間
+   * 導入 / Signal
+   * Solid の Signal は、前回の値を使って次の値を設定する関数形式も受け付けています。
+   * 最後に、JSX コードで Signal を読み取る必要があります:
    */
   // https://www.solidjs.com/tutorial/introduction_signals?solved
   const [count, setCount] = createSignal(0);
@@ -29,15 +31,37 @@ function HelloWorld() {
 
   /**
    * 導入 / Effect
+   * 開発者が createEffect で作成した Effect は、レンダリングが完了した後に実行され、主に DOM と相互作用する更新をスケジューリングするために使用されます。早めに DOM を変更したい場合は createRenderEffect を使用してください。
    */
   // https://www.solidjs.com/tutorial/introduction_effects
+  const [clickcount, setclickCount] = createSignal(0);
+
+  createEffect(() => {
+    console.log("The count is now", clickcount());
+  });
+
+  /**
+   * 派生 / Signal doubleCount
+   */
+   const doubleCount = () => count() * 2;
+
+  /**
+   * 導入 / MEMO
+   * https://www.solidjs.com/tutorial/introduction_memos
+   */
+
 
   return (
     <>
       <div>Hello {name}!</div>
       <Nested />
       {svg}
+      <h3>導入 / Signal</h3>
       <div>Count: {count()}</div>
+      <h3>導入 / Effect</h3>
+      <button onClick={() => setclickCount(clickcount() + 1)}>Click Me</button>
+      <h3>導入 / 派生 / Signal</h3>
+      <div>Count: {doubleCount()}</div>
     </>
   )
 }
