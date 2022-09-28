031b7c0a70b6efdd0d6591b498e76664a7260cc7
uemu5683 <nobuyoshi5683@gmail.com>
2022-09-29 02:01:34 +0900

    学習

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,4 +1,6 @@
 /* @refresh reload */
+import logo from './../logo.svg';
+import styles from './../App.module.css';
 import { render } from 'solid-js/web';
 import "../assets/css/styles.css";
 import "../assets/css/top.css";
@@ -7,16 +9,19 @@ function TopIndex() {
   return (
     <>
       <div class="toppage">
-        <ul>
-          <li><a href="/basic.html" target="_blank">基本</a></li>
-          <li><a href="/flow.html" target="_blank">制御フロー</a></li>
-          <li><a href="/circle.html" target="_blank">サイクル</a></li>
-          <li><a href="/binding.html" target="_blank">バインディング</a></li>
-          <li><a href="/props.html" target="_blank">Props</a></li>
-          <li><a href="/store.html" target="_blank">ストア</a></li>
-          <li><a href="/reactivity.html" target="_blank">リアクティビティ</a></li>
-          <li><a href="https://www.solidjs.com/" target="_blank">Solidjs</a></li>
-        </ul>
+        <div class="toppage_inner">
+          <img src={logo} class={styles.logo} alt="logo" />
+          <ul>
+            <li><a href="/basic.html" target="_blank">基本</a></li>
+            <li><a href="/flow.html" target="_blank">制御フロー</a></li>
+            <li><a href="/circle.html" target="_blank">サイクル</a></li>
+            <li><a href="/binding.html" target="_blank">バインディング</a></li>
+            <li><a href="/props.html" target="_blank">Props</a></li>
+            <li><a href="/store.html" target="_blank">ストア</a></li>
+            <li><a href="/reactivity.html" target="_blank">リアクティビティ</a></li>
+            <li><a href="https://www.solidjs.com/" target="_blank">Solidjs</a></li>
+          </ul>
+        </div>
       </div>
     </>
   )
--- src/jsx/reactivity.jsx
+++ src/jsx/reactivity.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { createSignal, batch } from "solid-js";
+import { createSignal, batch, createEffect, untrack, on } from "solid-js";
 import "../assets/css/styles.css";
 
 const App = () => {
@@ -9,26 +9,52 @@ const App = () => {
    * https://www.solidjs.com/tutorial/reactivity_batch
    * リアクティビティ / バッチ更新
    */
-   const [firstName, setFirstName] = createSignal("John");
-   const [lastName, setLastName] = createSignal("Smith");
-   const fullName = () => {
-     console.log("Running FullName");
-     return `${firstName()} ${lastName()}`
-   } 
-   const updateNames = () => {
-     console.log("Button Clicked");
-     batch(() => {
+  const [firstName, setFirstName] = createSignal("John");
+  const [lastName, setLastName] = createSignal("Smith");
+  const fullName = () => {
+    console.log("Running FullName");
+    return `${firstName()} ${lastName()}`
+  } 
+  const updateNames = () => {
+    console.log("Button Clicked");
+    batch(() => {
       setFirstName(firstName() + "n");
       setLastName(lastName() + "!");
     })
-   }
+  }
 
+  /**
+   * https://www.solidjs.com/tutorial/reactivity_untrack
+   * リアクティビティ / untrack
+   */
+  const [a, setA] = createSignal(1);
+  const [b, setB] = createSignal(1);
+
+  createEffect(() => {
+    console.log(a(), untrack(b));
+  });
+
+  /**
+   * https://www.solidjs.com/tutorial/reactivity_on
+   * リアクティビティ / on
+   */
+  const [ona, onsetA] = createSignal(1);
+  const [onb, onsetB] = createSignal(1);
+
+  createEffect(on(ona, (ona) => {
+    console.log(ona, onb());
+  }, { defer: true }));
 
   return (
     <>
-      <h3>リアクティビティ</h3>
+      <h3>リアクティビティ / Batch</h3>
       <button onClick={updateNames}>My name is {fullName()}</button>
-
+      <h3>リアクティビティ / untrack</h3>
+      <button onClick={() => setA(a() + 1)}>Increment A</button>
+      <button onClick={() => setB(b() + 1)}>Increment B</button>
+      <h3>リアクティビティ / on</h3>
+      <button onClick={() => onsetA(ona() + 1)}>Increment A</button>
+      <button onClick={() => onsetB(onb() + 1)}>Increment B</button>
     </>
   )
 }
